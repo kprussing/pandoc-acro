9a85c2c76c978ff4b7fb83cbf65bec33bf08d7ea
kprussing <kprussing74@gmail.com>
2020-09-14 00:59:43 -0400

    Drop Python 3.5
    The f-string syntax was introduced in Python 3.6.  I could just use the
    old format syntax, but I don't use Pythons older than 3.6 so I see no
    reason to support it.

--- setup.py
+++ setup.py
@@ -100,7 +100,6 @@ setup(
         # that you indicate you support Python 3. These classifiers are *not*
         # checked by 'pip install'. See instead 'python_requires' below.
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
