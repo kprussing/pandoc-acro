c2005a8ac1ce53a3bbe9ea5e5c09cbb88ad5ff38
kprussing <kprussing74@gmail.com>
2020-12-16 23:36:03 -0500

    Check the punctuation returns the proper value
    While we are checking that the key gets properly parsed, we also need to
    check that the punctuation gets properly returned.  This is just one
    more check in the test_get_key function.  But, we have to be careful
    because we have to check if we are within a span and we should actually
    have punctuation.  We also have to deal with the conversion of the
    single quote to the fancy style.

--- tests/test_get_key.py
+++ tests/test_get_key.py
@@ -1,11 +1,38 @@
 __doc__ = """Check the get_key method works as expected"""
 
 import itertools
+import re
 
 import pytest
 
 import panflute
 import pandocacro
+
+key = "mwe"
+acronyms = {
+        "short": "short",
+        "long": "long"
+    }
+markups = (
+        f"+{key}",
+        f"[+{key}]{{}}",
+        f"[+{key}]{{.short}}"
+    )
+punctuations = (
+        "",
+        ".",
+        "'s",
+        "’s",
+    )
+text = f"""---
+acronyms:
+    {key}:
+        short: {acronyms['short']}
+        long: {acronyms['long']}
+...
+
+{{mark}}
+"""
 
 
 def test_get_key() -> None:
@@ -16,33 +43,28 @@ def test_get_key() -> None:
     punctuation.
 
     """
-    key = "mwe"
-    markups = (
-            f"+{key}",
-            f"[+{key}]{{}}",
-            f"[+{key}]{{.short}}"
-        )
-    contexts = (
-            "{markup}",
-            "{markup}.",
-            "{markup}'s",
-        )
-    for markup, context in itertools.product(markups, contexts):
-        mark = context.format(markup=markup)
-        doc = panflute.convert_text(f"""---
-acronyms:
-  {key}:
-    short: short
-    long: long
-...
-{mark}
-    """, standalone=True)
+    for markup, punc in itertools.product(markups, punctuations):
+        mark = markup + punc
+        doc = panflute.convert_text(text.format(mark=mark), standalone=True)
         elem = doc.content[0].content[0]
         result, post = pandocacro.get_key(elem, doc)
         if result != key:
             delim = '"' if "'" in mark else "'"
             pytest.fail(f"Error extracting {key} from {delim}{mark}{delim}. "
                         f"Found {elem} -> {result}")
+
+        if "[" not in markup:
+            # We have a bare string
+            pass
+        elif punc != "":
+            # We have a span so the punctuation should be the next
+            # element.
+            post = panflute.stringify(doc.content[0].content[1])
+
+        if punc != (re.sub("’", "'", post) if "'" in punc else punc):
+            delim = '"' if "'" in mark else "'"
+            pytest.fail(f"Error extracting {delim}{punc}{delim} "
+                        f"from {delim}{elem}{delim}")
 
 
 if __name__ == "__main__":
