79d19823d941d4802dc4c31d4d33d03db4c54050
kprussing <kprussing74@gmail.com>
2020-12-25 21:51:57 -0500

    Move key parsing into the initialization
    Instead of having the key only know about the base contents, it would be
    better to have it fully parse the details.  This prevents us from having
    to duplicate details in multiple locations.  For now, this boils down to
    updating the `get` method to fully extract the key details without
    changing the bare results.

--- pandocacro/keys.py
+++ pandocacro/keys.py
@@ -4,7 +4,7 @@ element.
 
 import re
 
-from typing import ClassVar, Optional, Pattern
+from typing import ClassVar, Match, Optional, Pattern, Set
 
 import panflute
 
@@ -24,10 +24,22 @@ class Key:
 
     value: str
         The acronym key.
-    count: bool
-        The entry should be counted (False if '*' and True otherwise)
-    post: str
+    count: bool, optional
+        The entry should be counted (False if '*' and True otherwise).
+    type: str, in {"full", "short", "long"}, optional
+        The version of the acronym to typeset.
+    capitalize: bool, optional
+        Capitalize the first work of the acronym.
+    plural: bool, optional
+        Use the plural form of the acronym.
+    post: str, optional
         The trailing punctuation.
+
+    Arguments
+    ---------
+
+    elem: :class:`panflute.Element`
+        The document element from which to extract the acronym key.
 
     """
 
@@ -36,10 +48,108 @@ class Key:
     )
     """The pattern for extracting a key"""
 
-    def __init__(self, value: str, count: str = "*", post: str = ""):
-        self.value: str = value
-        self.count: bool = count != "*"
-        self.post: str = post
+    TYPES: ClassVar[Set] = set((
+            "full",
+            "long",
+            "short",
+        ))
+    """The valid class options for the type of acronym expansion"""
+
+    def __init__(self, elem: Optional[panflute.Element] = None):
+        self.value: str = ""
+        self.count: bool = True
+        self.type: str = ""
+        self.capitalize: bool = False
+        self.plural: bool = False
+        self.post: str = ""
+        if elem is not None:
+            self.parse(elem)
+
+    @staticmethod
+    def match(elem: panflute.Element) -> Optional[Match[str]]:
+        """Pattern match for a key in the element
+
+        We can convert a given element to a string and match it against
+        the known format for an acronym key.  This method manages
+        converting an element to a string, while appropriately stripping
+        unnecessary punctuation, and passing the result to the
+        :func:`re.match`.
+
+        Parameters
+        ----------
+
+        elem: :class:`panflute.Element`
+            The document element to inspect for an acronym key.
+
+        Returns
+        -------
+
+        Match:
+            The result of the :func:`re.match` call with the string
+            version of the element.
+
+        """
+        if isinstance(elem, panflute.Str):
+            content = panflute.stringify(elem)
+            if isinstance(elem.parent, panflute.Quoted):
+                content = re.sub("['\"]", "", content)
+
+        elif isinstance(elem, panflute.Span):
+            content = panflute.stringify(elem.content[0]) \
+                if len(elem.content) == 1 else ""
+        else:
+            content = ""
+
+        return Key.PATTERN.match(content)
+
+    def parse(self, elem: panflute.Element) -> None:
+        """Parse the key from a document element
+
+        This method does the low-level details of extracting the
+        relevant information from a document :class:`panflute.Element`.
+        It inspects the key for the ``+`` followed by the optional
+        “don't count” flag ``*`` followed by the actual key.  It then
+        extracts the class details to set the type, capitalization, and
+        plural details if the element has the appropriate classes
+        attribute.
+
+        Raises
+        ------
+
+        RuntimeError:
+            If the ``classes`` of the element contain more than one of
+            the valid ``TYPES``.
+
+        """
+        match = self.match(elem)
+        if not match:
+            return None
+
+        self.value = match.group("value")
+        self.count = match.groupdict().get("count", "") != "*"
+        types = [] if not hasattr(elem, "classes") \
+            else [c for c in elem.classes if c in self.TYPES]
+        if len(types) > 1:
+            name = type(self).__name__
+            raise RuntimeError(
+                f"'{name}.parse' Too many classes {types}"
+            )
+
+        self.type = "" if len(types) == 0 else types[0]
+        self.capitalize = hasattr(elem, "classes") and "caps" in elem.classes
+        self.plural = hasattr(elem, "classes") and "plural" in elem.classes
+        self.post = match.groupdict().get("post", "")
+
+    def __str__(self) -> str:
+        return "[+" + ("" if self.count else "*") \
+            + self.value \
+            + "]{" \
+            + " ".join([
+                    (".plural" if self.plural else ""),
+                    (".caps" if self.capitalize else ""),
+                    ("." + self.type if self.type != "" else ""),
+            ]) \
+            + "}" + self.post
 
 
 def get(elem: panflute.Element, doc: panflute.Doc) -> Optional[Key]:
@@ -66,27 +176,9 @@ def get(elem: panflute.Element, doc: panflute.Doc) -> Optional[Key]:
     if "acronyms" not in doc.metadata:
         return None
 
-    if isinstance(elem, panflute.Str):
-        content = panflute.stringify(elem)
-        if isinstance(elem.parent, panflute.Quoted):
-            content = re.sub("['\"]", "", content)
-
-    elif isinstance(elem, panflute.Span):
-        if len(elem.content) > 1:
-            return None
-
-        content = panflute.stringify(elem.content[0])
-    else:
-        return None
-
-    if re.search(r"\s", content):
-        # Keys cannot have white space, but panflute should have already
-        # split this up.
-        return None
-
-    match = Key.PATTERN.match(content)
+    match = Key.match(elem)
     if not match:
         return None
 
-    key = Key(**match.groupdict())
+    key = Key(elem)
     return key if key.value in doc.metadata["acronyms"] else None
