a3c7d2e309b40985b676b53b58649c47fcd54e72
kprussing <kprussing74@gmail.com>
2021-02-06 23:09:58 -0500

    Allow flags for pytest
    Instead of always running all tests, we can use nox's positional
    argument parsing to pass alternate flags to pytest like an individual
    test we care about.

--- noxfile.py
+++ noxfile.py
@@ -18,10 +18,19 @@ def mypy(session):
 @nox.session(python=[f"3.{x}" for x in range(6, 10)],
              venv_backend="conda")
 def test(session):
-    """Run the regression tests"""
+    """Run the regression tests
+
+    Alternate flags can be passed to ``pytest`` using the positional
+    arguments.
+    """
     session.install("pytest")
     session.install("panflute>=2.0")
     session.install("pyyaml")
     session.install('.')
     session.conda_install("pandoc>=2.11")
-    session.run("pytest", "tests")
+    if session.posargs:
+        tests = session.posargs
+    else:
+        tests = ["tests"]
+
+    session.run("pytest", *tests)
