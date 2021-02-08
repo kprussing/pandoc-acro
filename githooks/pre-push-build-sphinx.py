783b159e46c93bcec952b2c09d3a0b27f6089884
kprussing <kprussing74@gmail.com>
2021-02-08 01:24:37 -0500

    Add a hook to build the docs when pushing to stable
    A major point of working with this filter is to explore some new work
    flows.  This script takes care of making sure the HTML documentation is
    up to date by running Sphinx and adding the documents if any changes
    occurred.  This only happens when the stable branch is being pushed and
    it refuses to push if there are any lingering or staged changes.  This
    is to prevent interfering with the user's state.  It will also checkout
    the stable branch, do it's thing, and then change back to the original
    branch.
    
    To use this script, copy it to .git/hooks/pre-push and make it
    executable.

---
+++ githooks/pre-push-build-sphinx.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+__doc__ = """Force build the HTML documentation if pushing to the stable
+branch.  This script checks if the stable branch is being pushed.  If it
+is, it checks it out, runs Sphinx to build the HTML documentation, adds
+and commits the changes if necessary, and reverts to the original
+branch.  It refuses to do any of this if there are uncommitted or staged
+changes as this would potentially interfere with the user's state.  To
+enable this hook, copy this file to ``.git/hooks/pre-push`` and make it
+executable with ``chmod +x .git/hooks/pre-push``.
+"""
+
+import argparse
+import pathlib
+import re
+import shutil
+import subprocess
+import sys
+import tempfile
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("remote", type=str,
+                    help="Name of the remote to which the push is being done")
+parser.add_argument("url", type=str,
+                    help="URL to which the push is being done")
+args = parser.parse_args()
+
+# Walk up from the current file until we hit a basename starting with
+# ".git".  At that point, the content of root should be the root of the
+# git repository
+root = pathlib.Path(__file__).resolve()
+while True:
+    if re.match("[.]git", root.name):
+        root = root.parent
+        break
+
+    root = root.parent
+
+pushing_stable = False
+for line in sys.stdin.readlines():
+    if all(re.search("stable$", x) for x in line.split()[::2]):
+        pushing_stable = True
+
+if not pushing_stable:
+    sys.exit(1)
+
+changes = subprocess.run(["git", "status", "--porcelain"],
+                         check=True,
+                         capture_output=True,
+                         universal_newlines=True).stdout
+if any(re.match(r"^\s*M", x) for x in changes.splitlines()):
+    print("Push error: You cannot push stable with modified files",
+          file=sys.stderr)
+    sys.exit(1)
+
+staged = subprocess.run(["git", "diff", "--cached", "--name-only"],
+                        check=True,
+                        capture_output=True,
+                        universal_newlines=True).stdout
+if staged != "":
+    print("Push error: You cannot push stable with staged files",
+          file=sys.stderr)
+    sys.exit(1)
+
+branch = subprocess.run(["git", "symbolic-ref", "HEAD"],
+                        check=True,
+                        capture_output=True,
+                        universal_newlines=True).stdout
+
+if not re.search("stable$", branch):
+    subprocess.run(["git", "checkout", "stable"], check=True)
+
+with tempfile.TemporaryDirectory() as tmp:
+    build = subprocess.run(
+        ["sphinx-build", "-M", "html", root / "docsrc", tmp]
+    )
+    success = build.returncode == 0
+    if success:
+        try:
+            shutil.rmtree(root / "docs")
+        except Exception as err:
+            print(f"Push error: Failed to remove docs with error {err}",
+                  file=sys.stderr)
+            success = False
+        else:
+            html = pathlib.Path(tmp) / "html"
+            (html / ".buildinfo").unlink()
+            html.rename(root / "docs")
+
+if success:
+    subprocess.run(["git", "checkout", root / "docs" / ".nojekyll"])
+    changes = subprocess.run(["git", "status", "--porcelain"],
+                             capture_output=True,
+                             universal_newlines=True).stdout
+    if any(re.match(r"^\s*M", x) for x in changes.splitlines()):
+        subprocess.run(["git", "add", root / "docs"])
+        subprocess.run(["git", "commit", "-m", "Automatic push HTML Update"])
+
+else:
+    subprocess.run(["git", "checkout", root / "docs"])
+
+if not re.search("stable$", branch):
+    subprocess.run(["git", "checkout", branch], check=True)
+
+sys.exit(0 if success else 1)
